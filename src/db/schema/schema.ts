0e81cdcc2e525ddd32ac5040e9da970fa568a157
Daryl <daryl@Daryls-MacBook-Pro.local>
2026-03-04 22:18:35 +0800

    Initialize: Proper file structure, schema, relations, first insert API to DB

---
+++ src/db/schema/schema.ts
@@ -0,0 +1,50 @@
+import { relations } from 'drizzle-orm';
+import { pgTable,  varchar, timestamp, integer } from 'drizzle-orm/pg-core';
+
+const timestamps = {
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().$onUpdate(()=> new Date()).notNull()
+}
+
+export const departments = pgTable('departments', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  code: varchar ('code',{length:50}).notNull().unique(),
+  name: varchar ('name',{length:255}).notNull(),
+  description: varchar ('description',{length:255}),
+  ...timestamps
+});
+//When trying to delete a department, if any subject depends on it, don’t allow deletion (restrict).
+export const subjects = pgTable('subjects', {
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
+  department_id: integer('department_id').notNull().references(() => departments.id,{onDelete: 'restrict'}),
+  name: varchar ('name',{length:255}).notNull(),
+  code: varchar ('code',{length:50}).notNull().unique(),
+  description: varchar ('name',{length:255}),
+  ...timestamps
+});
+
+
+//Relations
+//technically a type checker for schema
+//Without this line, if you fetch a department, you just get the dept info
+//With this line you can easily ask for the database to include all subjects of a dept in one go
+export const departmentRelations = relations(departments, ({many})=>({subjects: many(subjects) }))
+
+//Subject Relations
+export const subjectRelations = relations(subjects, ({one})=>({
+  department: one(departments, {
+  fields: [subjects.department_id],
+  references: [departments.id]
+  }) 
+}))
+
+
+//automatically create types for you
+//based on your schema
+//to avoid manual typing of types
+export type Department = typeof departments.$inferSelect;
+export type NewDepartment = typeof departments.$inferInsert;
+
+//Subject
+export type Subject = typeof subjects.$inferSelect;
+export type NewSubject = typeof subjects.$inferInsert;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import express from 'express';
 
-import { db } from './db';
-import { cars } from './db/schema';
-
+import { db } from './db'
 //new instance
 const app = express();
 const port = 3000;
 
 const router = express.Router();
 
-
+import { departments } from './db/schema'
 
 //essential middleware function
 app.use(express.json());
@@ -30,17 +28,72 @@ app.get('/', (req, res) => {
     res.send('Hello from the CARS API')
 })
 
+// router.get('/:id',(req,res)=>{
+//     //everything we get from the params are string
+//     const id = Number(req.params.id);
+
+//     const car = cars.find((car)=> car.id === id);
+
+//     if(!car){
+//         return res.status(404).send('Car not found')
+//     }
+
+//     res.json(car)
+
+    
+// })
+
 router.get('/', async(req,res)=>{
-    const result = await db.select().from(cars);
+    const result = await db.select().from(departments);
     res.json(result); 
 })
 
+router.post('/', async (req,res)=>{
+    const {code, name, description} = req.body;
+    if(!code || !name || !description){
+        res.status(400).json({error: "Missing fields"})
+    }
+
+    const result= await db.insert(departments).values({code,name,description}).returning();
+
+    res.json(result); 
+})
+
+// router.put('/:id',(req,res)=>{
+//   const id = Number(req.params.id);
+//   const car = cars.find(car => car.id === id);
+
+//   if (!car) {
+//     return res.status(404).send('Car not found');
+//   }
+
+//   const allowedFields = ['make', 'model', 'year', 'price'];
+
+//   allowedFields.forEach(field => {
+//     if (req.body[field] !== undefined) {
+//       car[field] = req.body[field];
+//     }
+//   });
+
+//   res.json(car);
+// })
+
+// router.delete('/:id',(req,res)=>{
+//     const id = Number(req.params.id);
+//     const index = cars.findIndex((car)=>car.id===id);
+
+//     if(index===-1){
+//         res.status(404).json({error:"Car not found"})
+//     }
+
+//     const deleted = cars.splice(index, 1)[0];
+//     res.status(201).json({message:"Car Deleted", details: deleted})
+// })
 
 
 
 
 
-
-app.use('/api/v1/aramis',router);
+app.use('/api/aramis/departments',router);
 
 app.listen(port, ()=>console.log(`Server is running on http://localhost:${port}`))
